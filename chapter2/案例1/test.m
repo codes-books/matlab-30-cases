f9c3e3383162c7449f6d2c0faf5cada0201bc554
luke <lukke@qq.com>
2022-09-02 18:19:19 +0800

    test

--- chapter2/案例1/test.m
+++ chapter2/案例1/test.m
@@ -1,13 +1,17 @@
-function flag=test(lenchrom,bound,code)
-% lenchrom   input : 染色体长度
+function flag = test(~, bound,code)
+% 用于检验染色体的可行性
+
+% lenchrom(~)input : 染色体长度
 % bound      input : 变量的取值范围
 % code       output: 染色体的编码值
 
-flag=1;
-[n,m]=size(code);
+% 对于定义或存在但是未被使用的变量命令为 ~
 
-for i=1:n
-    if code(i)<bound(i,1) || code(i)>bound(i,2)
-        flag=0;
+flag = 1;
+[n,~] = size(code);
+
+for i = 1:n
+    if code(i) < bound(i,1) || code(i) > bound(i,2)
+        flag = 0;
     end
 end
